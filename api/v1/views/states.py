25bf0ee4fcc6bc8cec0ce28b8f3d06c66af843a2
kedabudre <abdurehimk77@gmail.com>
2024-06-12 02:07:44 +0300

    update file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -21,7 +21,7 @@ def getAllStates():
 @app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
 def GET_state(state_id):
     """GET State object, else raise 404"""
-    state = storage.get(State, state_id)  # Corrected: Use State class directly
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
@@ -30,13 +30,13 @@ def GET_state(state_id):
 @app_views.route('/states/<state_id>', strict_slashes=False, methods=['DELETE'])
 def DEL_state(state_id):
     """Delete a state object"""
-    state = storage.get(State, state_id)  # Corrected: Use State class directly
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
 
     storage.delete(state)
     storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
@@ -54,13 +54,13 @@ def POST_state():
     # send in user input(key:value) to create new object
     new_state = State(**post_content)
     new_state.save()
-    return jsonify(new_state.to_dict()), 201
+    return make_response(jsonify(new_state.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def PUT_state(state_id):
     """Updates a state object"""
-    state = storage.get(State, state_id)  # Corrected: Use State class directly
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
 
