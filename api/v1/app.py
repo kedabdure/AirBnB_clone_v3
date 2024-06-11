19c75e10d7824ef4696f97b66bb1378e43139b8d
kedabudre <abdurehimk77@gmail.com>
2024-06-11 09:20:57 +0300

    update file

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,16 +4,24 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, render_template, make_response, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+
 @app.teardown_appcontext
 def teardown_db(error):
     """ Close Storage """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """resource was not found"""
+    return make_response(jsonify({'error': "Not found"}), 404)
+
 
 if __name__ == "__main__":
     """ Main Function """
