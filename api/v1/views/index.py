558b55595329b6e5061acab757fe605670321bf0
kedabudre <abdurehimk77@gmail.com>
2024-06-11 04:14:53 +0300

    update file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 """index file"""
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from flask import jsonify
-from api.v1.views import app_views
-from models import storage
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
 
-
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def no_obj():
+def stats():
     """return the number of each object"""
-    no_objs = {
+    no_obj = {
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
         "places": storage.count(Place),
@@ -28,4 +26,4 @@ def no_obj():
         "states": storage.count(State),
         "users": storage.count(User)
     }
-    return jsonify(no_objs)
+    return jsonify(no_obj)
