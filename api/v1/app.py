6c7820f6eae8037ab4680d7d127e38abb435b167
kedabudre <abdurehimk77@gmail.com>
2024-06-11 12:50:14 +0300

    create state view

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """api module"""
-
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -31,4 +30,4 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if not port:
         port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
