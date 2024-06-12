fed7a72a6c26428f38ffd141c120ce4520ad29da
kedabudre <abdurehimk77@gmail.com>
2024-06-12 03:17:20 +0300

    update file

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
 """
-New view for Amenity objects that handles all default Restful API actions
+Amenity file for API
 """
 from api.v1.views import app_views
-from flask import abort, jsonify, request
-from models.city import City
-from models.state import State
-from models import storage
+from flask import abort, jsonify, request, make_response
 from models.amenity import Amenity
+from models import storage
 
 
-@app_views.route('/amenities', methods=['GET'],
-                 strict_slashes=False)
-def ALL_Amenities():
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities():
     """Retrieves the list of all Amenity objects"""
-    retval = []
-    all_amenities = storage.all('Amenity')
-    for amenities in all_amenities.values():
-        retval.append(amenities.to_dict())
-    return jsonify(retval)
+    amenities = [
+        amenity.to_dict() for amenity in storage.all(Amenity).values()
+    ]
+    return jsonify(amenities)
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['GET'],
                  strict_slashes=False)
-def GET_Amenity(amenity_id):
-    """GET Amenity object, if amenity_id not linked - raise 404 """
-    amenity = storage.get("Amenity", amenity_id)
+def get_amenity(amenity_id):
+    """Retrieves an Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
     return jsonify(amenity.to_dict())
@@ -33,58 +29,53 @@ def GET_Amenity(amenity_id):
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
-def DEL_Amenity(amenity_id):
-    """DELETE amenity, raise 404 on error, returns 200 on success"""
-    amenity = storage.get("Amenity", amenity_id)
+def delete_amenity(amenity_id):
+    """Deletes an Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
 
-    amenity.delete()
+    storage.delete(amenity)
     storage.save()
-    storage.close()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-def POST_Amenity():
-    """Adds amenity, raise 400 upon error, returns 201 on success"""
-    post_amenity = request.get_json()
-
+def post_amenity():
+    """Creates an Amenity"""
     if not request.is_json:
-        abort(400, "Not a JSON")
+        abort(400, description="Not a JSON")
 
-    name = post_amenity.get('name')
-    if not name:
-        abort(400, "Missing name")
+    post_content = request.get_json()
+    name = post_content.get('name')
+    if name is None:
+        abort(400, description="Missing name")
 
-    new_amenity = Amenity(**post_amenity)
-    storage.new(new_amenity)
-
+    new_amenity = Amenity(**post_content)
     new_amenity.save()
-    storage.close()
-    return jsonify(new_amenity.to_dict()), 201
+    return make_response(jsonify(new_amenity.to_dict()), 201)
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
-def PUT_Amenity(amenity_id):
-    """Update amenity, raise 404 on error, 200 on success"""
-    amenity = storage.get("Amenity", amenity_id)
+def put_amenity(amenity_id):
+    """Updates an Amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
 
     ignore_keys = ["id", "created_at", "updated_at"]
 
-    amenity_info = request.get_json()
+    if not request.is_json:
+        abort(400, description="Not a JSON")
 
-    if not request.get_json():
-        abort(400, "Not a JSON")
+    content = request.get_json()
+    if content is None:
+        abort(400, description="Not a JSON")
 
-    for key, val in amenity_info.items():
+    for key, val in content.items():
         if key not in ignore_keys:
             setattr(amenity, key, val)
 
     amenity.save()
-    storage.close()
-
-    return jsonify(amenity.to_dict()), 200
+    return jsonify(amenity.to_dict())
